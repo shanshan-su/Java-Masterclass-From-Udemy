e9de22c1e42e67986f03f24bd4dfa9b3969e5d7e
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-07-24 22:13:06 -0500

    feat: followed along unit test intruction

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java-Masterclass-From-Udemy
---
+++ src/com/timbuchalka/BankAccountTest.java
@@ -0,0 +1,21 @@
+package com.timbuchalka;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() throws Exception {
+        fail("This test  has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void withdraw() throws Exception {
+        fail("This test  has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void getBalance() throws Exception {
+        fail("This test  has yet to be implemented");
+    }
+}
